a3c031f13edabaf66d4098417bd9e6f126c20f26
brendy <walterorlin@gmail.com>
2015-05-19 11:46:46 -0700

    Add pyqt dice roller Add make a lot work towards making work on dice roller.

--- pyqtdiceroll.py
+++ pyqtdiceroll.py
@@ -6,6 +6,7 @@ import sys
 
 random.seed()
 
+rollout=0
 
 class Window(QWidget):
     def __init__(self):
@@ -32,17 +33,28 @@ class Window(QWidget):
         button=QPushButton("d10")
         button.clicked.connect (self.roll_d10)
         layout.addWidget (button,0,3 )
+        
+        label=QLabel(str(rollout))
+        layout.addWidget(label)
+        
+               	
+    #def intLabelPrint(result,win):
+    #    str_result=str(result)
+    #    label=Qlabel(str_result)
+    #    layout.addWidget( label,1,0)
     #should add a GUI print function to add text to the rolls as a tally. 
     def roll_d20(self, button):
-            print( random.randint(1,20))
+           # resultlabel=QLabel(str(randint(1,20))
+	   # layout.addWidget(resultlabel,1,4) 
+        rollout= random.randint(1,20)
     def roll_d6 (self, button):
-           print( random.randint(1,6))
+        rollout= random.randint(1,6)
     def roll_d4 (self, button):
-           print(random.randint(1,4))
+        rollout=random.randint(1,4)
     def roll_d8 (self, button):
-           print (random.randint(1,8))
+        rollout=random.randint(1,8)
     def roll_d10 (self, button):
-           print( random.randint(1,10))
+        rollout=random.randint(1,10)
 app=QApplication (sys.argv)
 
 screen=Window()
