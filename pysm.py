4b384d3e5ba1ca4143f551a243219866e8e2f489
Brendan Perrine <walterorlin@gmail.com>
2015-06-29 12:38:08 -0700

    add simple I am taking a walk message

---
+++ pysm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+from PyQt5.QtCore  import * 
+from PyQt5.QtWidgets import *
+import sys 
+
+
+class Window (QWidget):
+    def __init__(self): 
+        super(Window, self).__init__()
+        QWidget.__init__(self)
+
+
+
+        layout=QGridLayout()
+        self.setLayout(layout)
+
+
+        label=QLabel(" I am talking a walk.")
+        layout.addWidget(label,0,0)
+
+
+
+app= QApplication(sys.argv)
+
+screen=Window()
+screen.show()
+screen.showFullScreen()   
+sys.exit(app.exec())
+
